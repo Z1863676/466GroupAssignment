671c3ccf0d7511766ebfa6bbb7554ed0d6e24cac
Z1863676 <70176906+Z1863676@users.noreply.github.com>
2020-12-02 19:29:54 -0600

    Add files via upload

--- 466Project/searchWorkout.php
+++ 466Project/searchWorkout.php
@@ -1,8 +1,32 @@
+<?php
+session_start();
+require('dbconn.php');
+?>
+
 <html>
 
 <head><title>466 Group Project</title></head>
 <body>
-Search Workouts!
+	<p>Please select the date of a workout to search</p>
+	<form action="query.php" method="POST">
+		<select name="workoutType">
+		<?php
+
+		$sql = 'SELECT workoutDate FROM Workout;';
+		$stmt = $conn->prepare( $sql );
+		$stmt->execute();
+
+		$result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+date:
+		foreach( $result as $row ) {
+			echo '<option>';
+			echo $row['workoutDate'];
+			echo '</option>';
+		}
+		?>
+		</select>
+		<input type="submit" name="searchWorkout" value="SUBMIT"/>
+	</form>
 </body>
 
 </html>
